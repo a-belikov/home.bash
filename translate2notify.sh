23da02bc697de91b137ce8e123f28493926c41db
Alexey Belikov <a@belikov.tech>
2019-04-18 10:28:06 +0300

    INIT

---
+++ translate2notify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# translate selected text and show it as notification
+# thanks to https://github.com/soimort/translate-shell
+
+# 1. get selected text
+# xclip -out -selection primary
+SELECTEDTEXT=$(xclip -out -selection primary)
+
+# 2. translate -b (brief mode) :ru (to russian language)
+# https://github.com/soimort/translate-shell
+# trans -b :ru "Hello"
+TRANSLATEDTEXT=$(trans -b :ru "$SELECTEDTEXT")
+
+# 3 send result as notify. icons from /usr/share/icons/gnome/32x32
+# notify-send --urgency=normal --icon=face-glasses --expire-time
+notify-send --urgency=normal --icon=face-glasses --expire-time 8000 "$TRANSLATEDTEXT"
